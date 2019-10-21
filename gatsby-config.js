25cb6b36e5736674eb03b1417ccdfedb67d85a4e
kempsterrrr <kempsterwilliam@gmail.com>
2019-10-21 21:47:38 +0100

    fixing production build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data`,
+        path: `${__dirname}/data`,
       },
     },
     {
